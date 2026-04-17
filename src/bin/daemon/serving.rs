08db4675866ae8185377f5e71e3ffbf5f58db720
dinikai <dinikaigd@gmail.com>
2026-04-17 22:20:57 +0700

    Use tokio task spawning

--- src/bin/daemon/serving.rs
+++ src/bin/daemon/serving.rs
@@ -8,8 +8,10 @@ use cake::{
     token_pool::HashedToken,
 };
 use std::io;
+use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::Mutex;
 
 /// Wraps a TCP listener with control methods.
 pub struct Server {
@@ -28,21 +30,33 @@ impl Server {
     /// Starts a loop within which will listen
     /// to the incoming requests.
     pub async fn start(self) -> Option<()> {
-        let mut config = Config::from_default().await.ok()?;
-        let mut token_pool = AuthTokenPool::from_default().await.ok()?;
+        let config = Config::from_default().await.ok()?;
+        let token_pool = AuthTokenPool::from_default().await.ok()?;
+
+        let config_mutex = Arc::new(Mutex::new(config));
+        let pool_mutex = Arc::new(Mutex::new(token_pool));
 
         log::info!("The server has been started successfully.");
 
         loop {
             let (mut stream, _) = self.listener.accept().await.ok()?;
 
-            if let Err(e) = Self::handle_connection(&mut stream, &mut config, &mut token_pool).await
-            {
-                // Fallback error signal.
-                stream.write_all(&FATAL_CODE.to_le_bytes()).await.unwrap();
+            let config_mutex = config_mutex.clone();
+            let pool_mutex = pool_mutex.clone();
 
-                log::error!("Fatal error, writing the FATAL_CODE: {e}");
-            };
+            tokio::spawn(async move {
+                let mut config = config_mutex.lock().await;
+                let mut token_pool = pool_mutex.lock().await;
+
+                if let Err(e) =
+                    Self::handle_connection(&mut stream, &mut config, &mut token_pool).await
+                {
+                    // Fallback error signal.
+                    stream.write_all(&FATAL_CODE.to_le_bytes()).await.unwrap();
+
+                    log::error!("Fatal error, writing the FATAL_CODE: {e}");
+                };
+            });
         }
     }
 
